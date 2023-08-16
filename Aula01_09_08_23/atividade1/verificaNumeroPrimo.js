71779e9bc1d64e6488739ef5593318c8a932bc58
Gustavo <80643129+gustavo-oliveiraa@users.noreply.github.com>
2023-08-16 19:55:58 -0300

    Update verificaNumeroPrimo.js

--- Aula01_09_08_23/atividade1/verificaNumeroPrimo.js
+++ Aula01_09_08_23/atividade1/verificaNumeroPrimo.js
@@ -1,1 +1,9 @@
-
+function numeroPrimo(num) {
+    for (var i = 2; i < num; i++) {
+      if(num % i === 0) {
+         return false;
+      }
+    }
+    return num !== 1;  
+  }
+  console.log( numeroPrimo(5) );
