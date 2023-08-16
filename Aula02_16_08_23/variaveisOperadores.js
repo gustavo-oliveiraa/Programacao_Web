c4481d25379d8144b1ef302bb25f083aab34420a
Gustavo <80643129+gustavo-oliveiraa@users.noreply.github.com>
2023-08-16 10:56:35 -0300

    Add files via upload

---
+++ Aula02_16_08_23/variaveisOperadores.js
@@ -0,0 +1,12 @@
+let num1 = 10;
+let num2 = 2;
+
+let resultA = num1 + num2;
+let resultS = num1 - num2;
+let resultM = num1 * num2;
+let resultD = num1 / num2;
+
+console.log(`Resultado da ${resultA} entre ${num1} e ${num2}`)
+console.log(`Resultado da ${resultS} entre ${num1} e ${num2}`)
+console.log(`Resultado da ${resultM} entre ${num1} e ${num2}`)
+console.log(`Resultado da ${resultD} entre ${num1} e ${num2}`)
