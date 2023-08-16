92e84a4e52467af0285503e5979753fc03ddffcc
Gustavo <80643129+gustavo-oliveiraa@users.noreply.github.com>
2023-08-16 19:56:06 -0300

    Delete Primo_ou_nao.js

--- Aula01_09_08_23/Primo_ou_nao.js
+++
@@ -1,9 +0,0 @@
-function numeroPrimo(num) {
-    for (var i = 2; i < num; i++) {
-      if(num % i === 0) {
-         return false;
-      }
-    }
-    return num !== 1;  
-  }
-  console.log( numeroPrimo(5) );
